1cc2693b2e2da898ad071ea63a2b28a746212c15
taramouse <nickykeyse@gmail.com>
2019-09-17 20:50:17 +0100

    Added font awesome for vue.

---
+++ src/components/FontAwesomeTest.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <font-awesome-icon icon="user-secret"
+                       size="6x"></font-awesome-icon>
+    <font-awesome-icon :icon="['fab', 'twitter' ]"
+                       size="6x"></font-awesome-icon>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,19 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// https://github.com/FortAwesome/vue-fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+library.add(faSpinner, faUserSecret, fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 
 new Vue({
   router,
