48328e7da3d0606d50852af4601c326979f41497
taramouse <nickykeyse@gmail.com>
2019-09-23 14:37:16 +0100

    New navbar component.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <b-navbar toggleable="sm"
-                type="dark"
-                variant="secondary"
-                fixed="top">
-        <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
-
-        <b-navbar-brand>Optimized4U</b-navbar-brand>
-
-        <b-collapse id="nav-text-collapse"
-                    is-nav>
-          <b-navbar-nav class="ml-auto pr-4">
-            <b-nav-item to="/">Home</b-nav-item>
-            <b-nav-item to="/blog">Blog</b-nav-item>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </div>
-
+    <navbar />
     <div class="container">
       <router-view />
     </div>
-
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nav">
+    <b-navbar toggleable="sm"
+              type="dark"
+              variant="secondary"
+              fixed="top">
+      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+
+      <b-navbar-brand>Optimized4U</b-navbar-brand>
+
+      <b-collapse id="nav-text-collapse"
+                  is-nav>
+        <b-navbar-nav class="ml-auto pr-4">
+          <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/blog">Blog</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+</style>
